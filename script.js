b2265023a871191f1ae0c1edc0beff361145b7fd
Luco <36002993+gost-boo@users.noreply.github.com>
2023-12-06 15:43:40 -0600

    Remake
    Testing

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
-const canvasSize = 600;
-const grSize = canvasSize;
-function setup() {
-  createCanvas(canvasSize, canvasSize);
-  frameRate(30);
-  pg = createGraphics(grSize, grSize);
+let animationContainer = document.querySelector('.animated-title');
+let textData = animationContainer.getAttribute('aria-label');
+
+function splitWords() {
+  let splitedText = textData.split(' ');
+  
+  splitedText.join('& &').split('&').forEach(function(e){
+    let span = document.createElement('span');
+    span.classList.add('animated-word');
+    span.setAttribute('data-text', e); 
+    animationContainer.appendChild(span);
+  });
+  splitLetters();
+}
+splitWords()
+
+
+function splitLetters() {
+  let animatedWords = document.querySelectorAll('.animated-word');
+  animatedWords.forEach(function(e, i){
+    e.getAttribute('data-text').split('').forEach(function(f, j) {
+      f = f == ' ' ? '&#32;' : f;
+      let span = document.createElement('span');
+      span.classList.add('animated-element');
+      span.setAttribute('aria-hidden', 'true'); 
+      span.innerHTML = f;
+      e.appendChild(span);
+    });
+    animate(e, i);
+  })
 }
 
-function draw() {
-  //background(0);
-  pg.background(0);
-  pg.fill(255);
-  pg.textFont("Hind");
-  pg.textSize(canvasSize/2.5);
-  pg.push();
-  pg.translate(grSize/2, grSize/2);
-  pg.textAlign(CENTER, CENTER);
-  pg.textLeading(canvasSize/6.6-10);
-  pg.text("gost", -canvasSize/10.5, -canvasSize/30.25, 200);
-  pg.pop();
-
-  let tilesX = 50;
-  let tilesY = 6;
-
-  let tileW = int(width/tilesX);
-  let tileH = int(height/tilesY);
-
-  for (let y = 0; y < tilesY; y++) {
-    for (let x = 0; x < tilesX; x++) {
-
-      // WARP
-      let wave = int(sin(frameCount * 0.05 + ( x * y ) * 0.04) * 60);
-      //wave = 0;
-      // SOURCE
-      let sx = x*tileW + wave;
-      let sy = y*tileH + wave*1.25;
-      let sw = tileW;
-      let sh = tileH;
-
-
-      // DESTINATION
-      let dx = x*tileW;
-      let dy = y*tileH;
-      let dw = tileW;
-      let dh = tileH;
-
-      copy(pg, sx, sy, sw, sh, dx, dy, dw, dh);
-
-    }
-  }
+function animate(e, i) {
+  let wordCount = e.getAttribute('data-text').length;
+  e.style.opacity = 1;
+  e.classList.add('animate');
 }
